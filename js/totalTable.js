9966cfc0fca1b9c0767a3f047d63746849dfc7e7
Dynamic-Code <dynamic2106@gmail.com>
2021-04-29 21:49:00 +0530

    Added country wise data

--- js/totalTable.js
+++ js/totalTable.js
@@ -1,4 +1,5 @@
   var nf = new Intl.NumberFormat();
+  var cc
     $.get("https://api.coronatracker.com/v3/stats/worldometer/country",
     
     function (data){
@@ -7,13 +8,14 @@
         for(var i = 1; i<=data.length-4; i++){
 
          
-          var x = tval.insertRow();
+          var x = tval.insertRow()
           if(data[i-1]['countryCode'] != null )
           {
-          var CC = data[i-1]['countryCode'].toLowerCase();
+           CC = data[i-1]['countryCode'].toLowerCase();
        //   console.log(CC);
        //   var CC = countryCode.toLowerCase();
-            
+          x.setAttribute('onClick','window.location.assign("pages/countryWise.html?CC='+CC+'")');
+          
             x.insertCell(0);
 
             tval.rows[i].cells[0].innerHTML = i;
@@ -34,12 +36,22 @@
 
             tval.rows[i].cells[4].innerHTML = nf.format(data[i-1]['totalDeaths']);
 
-            $( ".rows" ).click(function() {
-              console.log(CC);
-            });
+            // $( ".rows" ).click(function() {
+            //   console.log(CC);
+            // });
 
           } 
+
+          // tval.rows[i].onclick(function(){
+          //   alert("The paragraph was clicked.");
+          // })
+
+
           
+        }
+
+        function myFunction() {
+          console.log('Hi');
         }
       }
     )
